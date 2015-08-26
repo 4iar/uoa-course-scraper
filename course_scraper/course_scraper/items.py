9470cfcefedb5ce35f75b152041c335e9c9cf2df
4iar <4iar@users.noreply.github.com>
2015-08-26 17:59:37 +0100

    Update item classes

--- course_scraper/course_scraper/items.py
+++ course_scraper/course_scraper/items.py
@@ -4,17 +4,22 @@ from scrapy.item import Item, Field
 
 class StudyType(Item):
     """Method of study (UG, PG, DL)"""
-    study_type = Field()
+    study_type = "UG"
 
 class Category(StudyType):
     category = Field()
+    category_url = Field()
 
 class Course(Category):
+    course_url = Field()
     level = Field()
     half = Field()
     credits = Field()
     code = Field()
     id = Field()
+    title = Field()
+    timetable_json_url = Field()
+    timetable_json = Field()
 
 
 
